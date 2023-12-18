e5ba91f0749903f63d678067fbbf034d20d3059f
TEareynare <153235897+TEareynare@users.noreply.github.com>
2023-12-18 14:16:22 -0600

    Add receive_alert function

--- src/blueprints/proactive_test_enablement.py
+++ src/blueprints/proactive_test_enablement.py
@@ -3,6 +3,12 @@ from flask import Flask, jsonify, request, Blueprint
 # Define blueprint
 proactive_test_enablement = Blueprint('proactive_test_enablement', __name__)
 
+
+# Routes and functions
+@proactive_test_enablement.route('/alert', methods = ['POST'])
+def receive_alert():
+    return "pong", 200
+
 @proactive_test_enablement.route('/ping', methods = ['GET'])
 def heatlh_check():
     return "pong", 200
