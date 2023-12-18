1dc7a2856688656fea7e0e589624cd9d3ba552ee
TEareynare <153235897+TEareynare@users.noreply.github.com>
2023-12-18 16:12:59 -0600

    Update enable_tests.py

--- src/commands/enable_tests.py
+++ src/commands/enable_tests.py
@@ -13,5 +13,5 @@ def enable_test(agents):
         enable_body = {"enabled": 'true'}
         headers = {"Authorization": "Bearer 05dd35b2-863a-469c-86da-99e74ba499d8"}
         enable_response = requests.put("https://api.thousandeyes.com/v7/tests/http-server/"+ test + "?aid=1129196", json=enable_body, headers=headers)
-        print("test enabled")
+        print("test enabled" + test)
     return
