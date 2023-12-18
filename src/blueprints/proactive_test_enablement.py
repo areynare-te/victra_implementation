4fc1a29c2dbef14002c8aa98af9c74a997d7bf75
TEareynare <153235897+TEareynare@users.noreply.github.com>
2023-12-18 14:18:40 -0600

    Changed alert route

--- src/blueprints/proactive_test_enablement.py
+++ src/blueprints/proactive_test_enablement.py
@@ -7,7 +7,7 @@ proactive_test_enablement = Blueprint('proactive_test_enablement', __name__)
 # Routes and functions
 @proactive_test_enablement.route('/alert', methods = ['POST'])
 def receive_alert():
-    return "pong", 200
+    return "This works", 200
 
 @proactive_test_enablement.route('/ping', methods = ['GET'])
 def heatlh_check():
