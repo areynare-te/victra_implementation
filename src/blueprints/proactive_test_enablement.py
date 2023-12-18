207b5d128d61faa07473e7b261deb8df2b18fdfe
TEareynare <153235897+TEareynare@users.noreply.github.com>
2023-12-18 14:25:35 -0600

    Added token

--- src/blueprints/proactive_test_enablement.py
+++ src/blueprints/proactive_test_enablement.py
@@ -7,7 +7,11 @@ proactive_test_enablement = Blueprint('proactive_test_enablement', __name__)
 # Routes and functions
 @proactive_test_enablement.route('/alert', methods = ['POST'])
 def receive_alert():
-    return "This works", 200
+    if "Authorization" in request.headers:
+        token = request.headers("Authorization").split(" ")[1]
+        return token, 200
+    else:
+        return "Unauthorized", 403
 
 @proactive_test_enablement.route('/ping', methods = ['GET'])
 def heatlh_check():
