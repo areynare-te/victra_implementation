609fb8aa6ede67e938167b6e3ab21b111205c35c
TEareynare <153235897+TEareynare@users.noreply.github.com>
2023-12-18 14:27:01 -0600

    Fix auth

--- src/blueprints/proactive_test_enablement.py
+++ src/blueprints/proactive_test_enablement.py
@@ -8,7 +8,7 @@ proactive_test_enablement = Blueprint('proactive_test_enablement', __name__)
 @proactive_test_enablement.route('/alert', methods = ['POST'])
 def receive_alert():
     if "Authorization" in request.headers:
-        token = request.headers("Authorization").split(" ")[1]
+        token = request.headers("authorization").split(" ")[1]
         return token, 200
     else:
         return "Unauthorized", 403
