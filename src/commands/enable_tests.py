6db69c456df13429ab35d2f9a2e3f311f3468f3d
TEareynare <153235897+TEareynare@users.noreply.github.com>
2023-12-18 20:41:56 -0600

    Update enable_tests.py

--- src/commands/enable_tests.py
+++ src/commands/enable_tests.py
@@ -4,7 +4,25 @@ test_relation = {
     "Alaska" : "4519688",
     "Bay Area" : "4520897",
     "California" : "4520906",
-    "Carolina": "4520919"
+    "Carolina": "4520919",
+    "Florida" : "4520920",
+    "Georgia" : "4520921",
+    "Hawaii" : "4520922",
+    "Iowa" : "4520923",
+    "Kansas" : "4520924",
+    "Michigan" : "4520925",
+    "MidWest" : "4520945",
+    "Midwest Central" : "4520953",
+    "Minnesota" : "4520954",
+    "NorthEast" : "4520955",
+    "NorthWest" : "4520956",
+    "Ohio" : "4520957",
+    "Pennsylvania" : "4520958",
+    "Southeast Central" : "4520959",
+    "SouthWest" : "4520960",
+    "Tenesse" : "4520968",
+    "Virginia" : "4520969",
+    "Wisconsin" : "4520970"
 }
 
 re_pattern = re.compile(r'Shops\s(.*?)(?=-)')
