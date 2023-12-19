01534218df991fbecc73bbdc8283e20d952eeff6
TEareynare <153235897+TEareynare@users.noreply.github.com>
2023-12-18 20:28:15 -0600

    Update enable_tests.py

--- src/commands/enable_tests.py
+++ src/commands/enable_tests.py
@@ -8,8 +8,8 @@ test_relation = {
 re_pattern = re.compile(r'Shops\s(.*?)(?=-)')
 
 def enable_test(alerted_test):
-    region = re.findall(re_pattern, alerted_test)
-    test = test_relation.get(region, '4520164')
+    region = re.search(re_pattern, alerted_test)
+    test = test_relation.get(region, '4519688')
     enable_body = {"enabled": 'true'}
     headers = {"Authorization": "Bearer 05dd35b2-863a-469c-86da-99e74ba499d8"}
     enable_response = requests.put("https://api.thousandeyes.com/v7/tests/http-server/"+ test + "?aid=1129196", json=enable_body, headers=headers)
